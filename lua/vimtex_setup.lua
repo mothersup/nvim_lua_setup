ae0e169b284af55a41a1d3291f867452c3d34529
mothersup <yhsha@connect.ust.com>
2022-03-05 18:14:52 +0800

    conflict: vimtex

--- lua/vimtex_setup.lua
+++ lua/vimtex_setup.lua
@@ -2,4 +2,4 @@ local g = vim.g
 
 g.vimtex_view_general_viewer = 'SumatraPDF'
 g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
-g.vimtex_view_general_options_latexmk = '-reuse-instance'
+-- g.vimtex_view_general_options_latexmk = '-reuse-instance'
