1ead09a4a75f0db7f5bc2f1644bd5306bf0a6a14
mothersup <shayh8262@gmail.com>
2022-09-20 10:07:44 +0800

    C

---
+++ lua/editing_setup/trevJ_setup.lua
@@ -0,0 +1,14 @@
+local trevj = require('trevj')
+
+trevj.setup({
+    containers = {
+        python = {
+            parameters = {
+                final_separator = false
+            },
+            argument_list = {
+                final_separator = false
+            }
+        }
+    }
+})
