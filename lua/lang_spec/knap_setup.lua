8a97ebb49bc27f8cd87d6f492b10c0d2c09c8fc8
mothersup <shayh8262@gmail.com>
2022-10-10 23:01:06 +0800

    A

--- lua/lang_spec/knap_setup.lua
+++ lua/lang_spec/knap_setup.lua
@@ -1,6 +1,7 @@
 vim.g.knap_settings = {
 	-- mdtohtmlviewerlaunch = "min %outputfile%",
-	textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchm.setup()ode %docroot%",
+	-- textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
+    textopdf = "arara %docroot%",
 	htmltohtmlviewerlaunch = "live-server --quiet --browser=min --open=%outputfile% --watch=%outputfile% --wait=800",
 	mdtohtmlviewerlaunch = "live-server --quiet --browser=min --open=%outputfile% --watch=%outputfile% --wait=800",
 	textopdfviewerlaunch = [[sioyek.exe --inverse-search 'nvim --headless ]]
