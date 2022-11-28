6a9bf1e7873cf58f96469363c5a8a1ed9ac04a9e
mothersup <shayh8262@gmail.com>
2022-11-28 13:13:50 +0800

    C

--- lua/lang_spec/knap_setup.lua
+++ lua/lang_spec/knap_setup.lua
@@ -1,18 +1,20 @@
 vim.g.knap_settings = {
     textopdf = "arara %docroot%",
-	-- mdtohtmlviewerlaunch = "min %outputfile%",
-	-- textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
-	-- textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchm.setup()ode %docroot%",
-	htmltohtmlviewerlaunch = "live-server --quiet --browser=min --open=%outputfile% --watch=%outputfile% --wait=800",
-	mdtohtmlviewerlaunch = "live-server --quiet --browser=min --open=%outputfile% --watch=%outputfile% --wait=800",
+    -- mdtohtmlviewerlaunch = "min %outputfile%",
+    -- textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchmode %docroot%",
+    -- textopdf = "lualatex -synctex=1 -halt-on-error -interaction=batchm.setup()ode %docroot%",
+    htmltohtmlviewerlaunch = [[live-server ]]
+        .. [[--quiet --browser=firefox --open=%outputfile% ]]
+        .. [[--watch=%outputfile% --wait=800]],
+    mdtohtmlviewerlaunch = "live-server --quiet --browser=firefox --open=%outputfile% --watch=%outputfile% --wait=800",
     -- textopdfviewerlaunch = "evince %outputfile%",
     -- textopdfviewerlaunch = "Sioyek-x86_64.AppImage %outputfile%",
     -- textopdfviewerrefresh= "none",
     -- textopdfviewerlaunch = "sioyek --reuse-instance %outputfile%"
-	textopdfviewerlaunch = [[sioyek.exe ]]
+    textopdfviewerlaunch = [[Sioyek-x86_64.AppImage ]]
         .. [[--inverse-search 'nvim --headless]]
-		.. [[-es --cmd "lua]]
-		.. [[require("knaphelper").relayjump]]
-		.. [[("%servername%", "%1", %2,%3)' --new-instance %outputfile% ]],
-	textopdfforwardjump = "sioyek --inverse-search 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%1'\"'\"',%2,%3)\"' --reuse-instance --forward-search-file %srcfile% --forward-search-line %line% %outputfile%",
+        .. [[-es --cmd "lua]]
+        .. [[require("knaphelper").relayjump]]
+        .. [[("%servername%", "%1", %2,%3)' --new-instance %outputfile% ]],
+    textopdfforwardjump = "Sioyek-x86_64.AppImage --inverse-search 'nvim --headless -es --cmd \"lua require('\"'\"'knaphelper'\"'\"').relayjump('\"'\"'%servername%'\"'\"','\"'\"'%1'\"'\"',%2,%3)\"' --reuse-instance --forward-search-file %srcfile% --forward-search-line %line% %outputfile%",
 }
